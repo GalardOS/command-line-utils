98f8081a9e501f9282bbf51b3f868897f10a8357
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-01 10:41:29 +0100

    Added basename and build mechanisms

---
+++ coreutils/basename/bin/Debug/main.d
@@ -0,0 +1,1 @@
+bin/Debug/main.o: src/main.cc
---
+++ coreutils/basename/src/main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+std::string get_basename(const std::string& name) {
+    for(int i = name.size() - 1; i > 0; i--) {
+        if(name[i] == '/') {
+            return name.substr(i + 1, name.size() - i);
+        }
+    }
+
+    return name;
+}
+
+int main(int argc, char** argv) {
+    // If no arguments are passed, program exits with error message.
+    if(argc < 2) {
+        std::cerr << "basename: Missing operand\n";
+        std::cerr << "  Try 'basename -h' for more information\n";
+    }
+
+    std::cout << get_basename(argv[1]) << std::endl;
+}
