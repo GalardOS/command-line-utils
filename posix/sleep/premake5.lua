381b6afded082b3601545f4cedfa4b2095430752
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-03-12 10:34:08 +0100

    Implementation

---
+++ posix/sleep/premake5.lua
@@ -0,0 +1,19 @@
+project "sleep"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+    targetdir("./")
+    objdir("bin/")
+
+    files {"src/**.cc", "src/**.hh"}
+    includedirs {"lib/include", "src"}
+
+    filter "configurations:Debug"
+        symbols "on"
+        optimize "off"
+    filter "configurations:Release"
+        optimize "on"
+        symbols "off"
+        runtime "Release"
