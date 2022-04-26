e4349e55cfda2fbaa066f95e2ceb4df80491a1b1
IkerGalardi <contacto.ikergalardi@gmail.com>
2022-04-26 17:25:31 +0200

    Added TODO on multiline echo

--- tests/echo.sh
+++ tests/echo.sh
@@ -6,7 +6,7 @@ NOCOLOR='\e[0m'
 
 test_echo() {
     RESULT=$(bin/echo $2)
-    if [ "$RESULT" = "$2" ]
+    if [ "$RESULT" = $3 ]
     then
         echo -e "  * Test $1:$GREEN passed $NOCOLOR"
     else
@@ -15,5 +15,7 @@ test_echo() {
     fi
 }
 
-test_echo 1 "kaixo"
-test_echo 2 "hello"
+test_echo 1 "kaixo" "kaixo"
+test_echo 2 "hello" "hello"
+
+# TODO: multiline echo, CAN'T test right now because i don't really know how to add a newline character inside the script
